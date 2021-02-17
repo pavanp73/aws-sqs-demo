c6acc811c9ab24a972cb315484da7442a9a7dfbb
Pavan Padmanabhi <11795349+pavanp73@users.noreply.github.com>
2021-02-18 00:09:30 +0530

    send and receive message from SQS (#1)

--- src/main/java/com/demo/aws/sqs/AwsSqsDemoApplication.java
+++ src/main/java/com/demo/aws/sqs/AwsSqsDemoApplication.java
@@ -1,13 +1,16 @@
 package com.demo.aws.sqs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AwsSqsDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsSqsDemoApplication.class, args);
+		log.info("application started...");
 	}
 
 }
